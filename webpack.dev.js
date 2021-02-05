de6c3a2baf173975221a414e6f3cc8d9c98339e1
ahmedachora10 <gjr8195@gmail.com>
2021-02-06 00:18:23 +0100

    Init envirement

---
+++ webpack.dev.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.pug"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.pug$/,
+        use: [
+          'pug-loader'
+        ]
+      }
+    ]
+  }
+});
